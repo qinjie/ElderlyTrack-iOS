19bb7d02a7bd2187b0c261eb81e5ad6d1962e237
Kyaw Lin <kyawlin@Kyaws-Mac.local>
2018-02-28 22:05:30 -0800

    Initial Commit

---
+++ WeTrack-Re/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeTrack-Re
+//
+//  Created by Kyaw Lin on 28/2/18.
+//  Copyright © 2018 Kyaw Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
